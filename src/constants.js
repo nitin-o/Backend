c7124f748b31786428dfa80bd358930f54b63858
Nitin Otavkar <nitinotavkar@gmail.com>
2026-04-23 01:04:06 +0530

    caneace DBsd

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const DB_Name = "mydb";
 const DB_URL ="mongodb+srv://nitinotavkar_db:nitin123@cluster0.aj64kdd.mongodb.net"
-const PORT = 3000
+const PORT = 4000
 
 
 export { DB_Name ,DB_URL ,PORT} 
